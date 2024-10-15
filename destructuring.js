0000c39bc513bc2d5bf0fece59d7908ba232135f
gausul9365 <khurrram9365@gmail.com>
2024-10-15 22:19:37 +0530

    destructuing

---
+++ destructuring.js
@@ -0,0 +1,60 @@
+
+// // // Destructing is used to unpack the value of the array and assign it to variable
+
+// // const [a,b] = [1,2];
+
+// // console.log(a);
+// // console.log(b);
+
+// // // Destructuring the array
+
+// // const array = [1,2,3,4,5];
+
+// // const [first, second] = array;
+
+// // console.log(first);
+// // console.log(second);
+
+// // // skipping the value
+
+// // const arr = [1,2,3,4,5,6,7];
+
+// // const [pahla,,,, panchwa] = arr;
+// // console.log(panchwa);
+ 
+// // // default value using the variable 
+
+// // const number = [1,2,3];
+
+// // const [pahila,,,chautha_num = 4] = number;
+// // console.log(chautha_num);
+
+// // // swaping vairiable
+
+// // let a_num = 13, b_num = 100;
+
+// // [a_num, b_num] = [b_num, a_num];
+
+// // console.log(a_num);
+// // console.log(b_num);
+
+// // // object Destructuring
+// // // object destructuring allows to extract properties frome objrct and assign it to the variables
+
+// // const [prop1,prop2] = object;
+
+// const person = {
+//   name: 'Alice',
+//   age: 25,
+//   job: 'Engineer'
+// };
+
+// // Destructure the object to extract properties
+// const { name, age, job } = person;
+
+// console.log(name);
+// console.log(age);  
+// console.log(job);  
+
+
+
