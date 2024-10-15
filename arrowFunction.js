8c16ec04a770fdb0b2f6b11ea583bc9984284834
gausul9365 <khurrram9365@gmail.com>
2024-10-15 22:29:06 +0530

    arrow

---
+++ arrowFunction.js
@@ -0,0 +1,14 @@
+khurram = function() {
+    return "hello";
+}
+
+
+hello = () => {
+  return "kuchh nhi";
+}
+
+
+// if there is one lines of code
+helloSir =()=> "kuchh nhi";
+
+hello = (val) => "Hello " + val;
